24c54df2e72351c69d5a1e5030b83b7b1b3b3249
chrisleholloway <47109301+chrisleholloway@users.noreply.github.com>
2019-01-28 14:20:48 -0500

    Update and rename power_mon.py to Shutdown.py

--- Shutdown.py
+++ Shutdown.py
@@ -1,16 +1,4 @@
-# Power Button monitor
-#
-# tctec.net
-#
-# Version 1.0
-# Modified 25-March-2016
-#
-#
-# Run this automatically in the background at startup.
-# one way to do this is with CRON by editing crontab
-# See: running a task at reboot using CRON
-#
-
+#!/usr/bin/env python3
 import RPi.GPIO as GPIO
 import time
 import os
@@ -18,18 +6,14 @@ import signal
 
 print "Power Button Monitor."
 
-
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
 
 def button_pressed(channel):
     print "Button pushed"
     os.system("sudo shutdown now")
     
-
 GPIO.add_event_detect(21, GPIO.RISING, callback=button_pressed, bouncetime=300)
-
 
 signal.pause()
