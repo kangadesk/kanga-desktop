164718cf3f3275df0c1898483ce51c19096fa9ee
chrisleholloway <47109301+chrisleholloway@users.noreply.github.com>
2019-01-28 14:55:07 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+#Run Root
+if [[ $EUID -ne 0 ]]; then
+   echo "Please execute as root." 
+   exit 1
+fi
+#
+
+#Update Repo
+sudo apt-get update -y
+#
+
+#gpiozero Module Install
+sudo apt-get install -y python3-gpiozero
+#
+
+#Download Python Script
+cd /opt/
+sudo mkdir KangaPi
+cd /opt/KangaPi
+script=Shutdown.py
+
+if [ -e $script ];
+	then
+		echo "Script Shutdown.py already exists. Doing nothing."
+	else
+		wget "https://raw.githubusercontent.com/kangadesk/kanga-pi/master/Shutdown.py"
+fi
+#
+
+#Auto Run
+cd /etc/
+RC=rc.local
+
+if grep -q "sudo python3 \/opt\/KangaPi\/Shutdown.py \&" "$RC";
+	then
+		echo "File /etc/rc.local already configured. Doing nothing."
+	else
+		sed -i -e "s/^exit 0/sudo python3 \/opt\/KangaPi\/Shutdown.py \&\n&/g" "$RC"
+		echo "File /etc/rc.local configured."
+fi
+#
+
+#Reboot
+echo "KangaPi installation complete. Enjoy! System will now reboot in 3 seconds."
+sleep 3
+sudo reboot
+#
