12e4096165aa61a1ad7a9dbb4d038935ef2a0580
chrisleholloway <47109301+chrisleholloway@users.noreply.github.com>
2019-01-28 18:02:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 #Run Root
 if [[ $EUID -ne 0 ]]; then
-   echo "Please execute as root." 
+   echo "Please run as root." 
    exit 1
 fi
 #
@@ -18,13 +17,13 @@ sudo apt-get install -y python3-gpiozero
 
 #Download Python Script
 cd /opt/
-sudo mkdir KangaPi
-cd /opt/KangaPi
-script=Shutdown.py
+sudo mkdir kangadesk/pi
+cd /opt/kangadesk/pi
+script=shutdown.py
 
 if [ -e $script ];
 	then
-		echo "Script Shutdown.py already exists. Doing nothing."
+		echo "Shutdown Script already exists. Doing nothing."
 	else
 		wget "https://raw.githubusercontent.com/kangadesk/kanga-pi/master/shutdown.py"
 fi
@@ -34,17 +33,17 @@ fi
 cd /etc/
 RC=rc.local
 
-if grep -q "sudo python3 \/opt\/KangaPi\/Shutdown.py \&" "$RC";
+if grep -q "sudo python3 \/opt\/kangadesk\/pi\/shutdown.py \&" "$RC";
 	then
 		echo "File /etc/rc.local already configured. Doing nothing."
 	else
-		sed -i -e "s/^exit 0/sudo python3 \/opt\/KangaPi\/shutdown.py \&\n&/g" "$RC"
+		sed -i -e "s/^exit 0/sudo python3 \/opt\/kangadesk\/pi\/shutdown.py \&\n&/g" "$RC"
 		echo "File /etc/rc.local configured."
 fi
 #
 
 #Reboot
-echo "Kangadesk installation complete. Enjoy! System will now reboot in 9 seconds."
-sleep 3
+echo "Kangadesk Pi Addons Install Complete. Enjoy! System will now reboot in 10 seconds."
+sleep 10
 sudo reboot
 #
