3bceb83a2aa2d64a306743f91880831b1527e526
chrisleholloway <47109301+chrisleholloway@users.noreply.github.com>
2019-01-29 01:58:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ sudo apt-get install -y python3-gpiozero
 cd /opt/
 sudo mkdir kangadesk
 cd /opt/kangadesk
-wget -O "https://raw.githubusercontent.com/kangadesk/kangadesk-mate/master/720677.jpg"
+wget "https://raw.githubusercontent.com/kangadesk/kangadesk-mate/master/720677.jpg"
 script=shutdown.py
 
 if [ -e $script ];
@@ -65,8 +65,7 @@ if grep -q "disable_splash=1" "$File";
 fi
 #
 
-#
-pcmanfm --set-wallpaper="/opt/kangadesk/720677.jpg"
+#pcmanfm --set-wallpaper="/opt/kangadesk/720677.jpg"
 sudo cp /opt/kangadesk/720677.jpg /usr/share/plymouth/themes/pix/splash.png
 #
 
