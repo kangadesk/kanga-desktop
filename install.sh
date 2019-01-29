332cc1f6c7d5b1dc5050991decaa6cf0a337cd6c
chrisleholloway <47109301+chrisleholloway@users.noreply.github.com>
2019-01-28 21:29:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,12 +45,12 @@ fi
 #Additional Steps
 cd /home/pi/
 sudo mkdir kangadesk
-cd /kangadesk
+cd /kangadesk/
 
 curl -O -O https://raw.githubusercontent.com/kangadesk/kangadesk-mate/master/README.md https://raw.githubusercontent.com/kangadesk/kangadesk-mate/master/720677.jpg
 
-pcmanfm --set-wallpaper /home/kangadesk/720677.jpg
-sudo cp /home/kangadesk/720677.jpg /usr/share/plymouth/themes/pix/splash.png
+pcmanfm --set-wallpaper /home/pi/kangadesk/720677.jpg
+sudo cp /home/pi/kangadesk/720677.jpg /usr/share/plymouth/themes/pix/splash.png
 #
 
 #Reboot
