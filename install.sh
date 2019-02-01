813472d1ab10f4d34609ad79626f501ca61bdb86
chrisleholloway <47109301+chrisleholloway@users.noreply.github.com>
2019-02-01 17:31:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget "https://raw.githubusercontent.com/kangadesk/kangadesk-mate/master/create.sh" -O /pi/create.sh
-chmod +x create.sh
+sudo cd /pi/
+sudo wget "https://raw.githubusercontent.com/kangadesk/kangadesk-mate/master/create.sh"
+sudo chmod +x create.sh
