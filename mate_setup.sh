98fc9d9b9761d9e3a03b6530cd372553e6d671bf
Team Kangadesk <47109301+teamkangadesk@users.noreply.github.com>
2019-02-02 02:46:08 -0500

    Update mate_setup.sh

--- mate_setup.sh
+++ mate_setup.sh
@@ -7,72 +7,57 @@ if [[ $EUID -ne 0 ]]; then
 fi
 #
 
-show_menu(){
-    NORMAL=`echo "\033[m"`
-    MENU=`echo "\033[36m"` #Blue
-    NUMBER=`echo "\033[33m"` #yellow
-    FGRED=`echo "\033[41m"`
-    RED_TEXT=`echo "\033[31m"`
-    ENTER_LINE=`echo "\033[33m"`
-    echo -e "${MENU}*********************************************${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 1)${MENU} Mount dropbox ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 2)${MENU} Mount USB 500 Gig Drive ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 3)${MENU} Restart Apache ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 4)${MENU} ssh Frost TomCat Server ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 5)${MENU} ${NORMAL}"
-    echo -e "${MENU}*********************************************${NORMAL}"
-    echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
-    read opt
+show_menu () {
+    # We show the host name right in the menu title so we know which Pi we are connected to
+    OPTION=$(whiptail --title "Menu (Host:$(hostname))" --menu "Choose your option:" 12 36 5 \
+    "1" "Current time" \
+    "2" "Calendar" \
+    "3" "Uptime" \
+    "4" "Reboot Pi" \
+    "5" "Shut down Pi"  3>&1 1>&2 2>&3)
+ 
+    BUTTON=$?
+    # Exit if user pressed cancel or escape
+    if [[ ($BUTTON -eq 1) || ($BUTTON -eq 255) ]]; then
+        exit 1
+    fi
+    if [ $BUTTON -eq 0 ]; then
+        case $OPTION in
+        1)
+            MSG="$(date)"
+            whiptail --title "Current time" --msgbox "$MSG" 8 36
+            show_menu
+            ;;
+        2)
+            # We use "ncal -h" to turn off highlighting of today's date since
+            # highlighting produces non-printable chars that don't look good.
+            whiptail --title "Calendar" --textbox /dev/stdin 13 26 <<<"$(ncal -bh)"
+            show_menu
+            ;;
+        3)
+            MSG="$(uptime)"
+            whiptail --title "Uptime info" --msgbox "$MSG" 8 36
+            show_menu
+            ;;
+        4)
+            # For sensitive commands, we make sure they must press extra keys
+            confirmAnswer "Are you sure you want to reboot the Pi?"
+            if [ $? = 0 ]; then
+                echo Rebooting...
+                sudo reboot
+            else
+                show_menu
+            fi
+            ;;
+        5)
+            confirmAnswer "Are you sure you want to shut down the Pi?"
+            if [ $? = 0 ]; then
+                echo Shutting down...
+                sudo poweroff
+            else
+                show_menu
+            fi
+            ;;
+        esac
+    fi  
 }
-function option_picked() {
-    COLOR='\033[01;31m' # bold red
-    RESET='\033[00;00m' # normal white
-    MESSAGE=${@:-"${RESET}Error: No message passed"}
-    echo -e "${COLOR}${MESSAGE}${RESET}"
-}
-
-clear
-show_menu
-while [ opt != '' ]
-    do
-    if [[ $opt = "" ]]; then 
-            exit;
-    else
-        case $opt in
-        1) clear;
-        option_picked "Option 1 Picked";
-        sudo mount /dev/sdh1 /mnt/DropBox/; #The 3 terabyte
-        menu;
-        ;;
-
-        2) clear;
-            option_picked "Option 2 Picked";
-            sudo mount /dev/sdi1 /mnt/usbDrive; #The 500 gig drive
-        menu;
-            ;;
-
-        3) clear;
-            option_picked "Option 3 Picked";
-        sudo service apache2 restart;
-            show_menu;
-            ;;
-
-        4) clear;
-            option_picked "Option 4 Picked";
-        ssh lmesser@ -p 2010;
-            show_menu;
-            ;;
-
-        x)exit;
-        ;;
-
-        \n)exit;
-        ;;
-
-        *)clear;
-        option_picked "Pick an option from the menu";
-        show_menu;
-        ;;
-    esac
-fi
-done
