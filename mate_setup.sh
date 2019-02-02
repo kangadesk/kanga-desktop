7e41df97b6055b346a19be58908a7e52d5574762
Team Kangadesk <47109301+teamkangadesk@users.noreply.github.com>
2019-02-02 02:50:14 -0500

    Update mate_setup.sh

--- mate_setup.sh
+++ mate_setup.sh
@@ -7,57 +7,32 @@ if [[ $EUID -ne 0 ]]; then
 fi
 #
 
-show_menu () {
-    # We show the host name right in the menu title so we know which Pi we are connected to
-    OPTION=$(whiptail --title "Menu (Host:$(hostname))" --menu "Choose your option:" 12 36 5 \
-    "1" "Current time" \
-    "2" "Calendar" \
-    "3" "Uptime" \
-    "4" "Reboot Pi" \
-    "5" "Shut down Pi"  3>&1 1>&2 2>&3)
- 
-    BUTTON=$?
-    # Exit if user pressed cancel or escape
-    if [[ ($BUTTON -eq 1) || ($BUTTON -eq 255) ]]; then
-        exit 1
-    fi
-    if [ $BUTTON -eq 0 ]; then
-        case $OPTION in
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+TITLE="Title here"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "Install Mate Addon Packages"
+         2 "Upgrade Exsisitng Packages"
+         3 "Configure Your Mate")
+
+CHOICE=$(dialog --clear \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
         1)
-            MSG="$(date)"
-            whiptail --title "Current time" --msgbox "$MSG" 8 36
-            show_menu
+            echo "You chose Option 1"
             ;;
         2)
-            # We use "ncal -h" to turn off highlighting of today's date since
-            # highlighting produces non-printable chars that don't look good.
-            whiptail --title "Calendar" --textbox /dev/stdin 13 26 <<<"$(ncal -bh)"
-            show_menu
+            echo "You chose Option 2"
             ;;
         3)
-            MSG="$(uptime)"
-            whiptail --title "Uptime info" --msgbox "$MSG" 8 36
-            show_menu
+            echo "You chose Option 3"
             ;;
-        4)
-            # For sensitive commands, we make sure they must press extra keys
-            confirmAnswer "Are you sure you want to reboot the Pi?"
-            if [ $? = 0 ]; then
-                echo Rebooting...
-                sudo reboot
-            else
-                show_menu
-            fi
-            ;;
-        5)
-            confirmAnswer "Are you sure you want to shut down the Pi?"
-            if [ $? = 0 ]; then
-                echo Shutting down...
-                sudo poweroff
-            else
-                show_menu
-            fi
-            ;;
-        esac
-    fi  
-}
+esac
