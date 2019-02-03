2d31a18bbbb78317988a89604e3c5ac14eb39f11
Team Kangadesk <47109301+teamkangadesk@users.noreply.github.com>
2019-02-03 01:02:03 -0500

    Update install-control.sh

--- packages/install-control.sh
+++ packages/install-control.sh
@@ -4,9 +4,12 @@ cd /opt/kangadesk/
 File="VERSION.md"
 if [ -d "$File" ]; 
   then
-                echo "Boot File already exists. Doing nothing."
+                sleep 5
+    		whiptail --title "Mate Setup Wizard" --msgbox "Mate Add-Ons Already Installed" 10 60
+    		wget -O - "https://raw.githubusercontent.com/kangadesk/mate-desktop/master/mate_setup.sh" | sudo bash
 	else
-		wget -q "https://raw.githubusercontent.com/kangadesk/mate-desktop/master/etc/systemd/system/boot.service"
-		echo "boot.service created."
+		sleep 5
+		whiptail --title "Mate Setup Wizard" --msgbox "Click OK to install Mate Add-Ons" 10 60
+		wget -O - "https://raw.githubusercontent.com/kangadesk/mate-desktop/master/packages/install.sh" | sudo bash
 fi
 #
