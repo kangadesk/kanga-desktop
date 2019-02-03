61ee4328b4f74eb841bbefe6d036a1fdd5ab894f
Team Kangadesk <47109301+teamkangadesk@users.noreply.github.com>
2019-02-03 00:46:49 -0500

    Update version-control.sh

--- packages/version-control.sh
+++ packages/version-control.sh
@@ -7,24 +7,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 #
 
-#Install Check
-cd /opt/kangadesk/
-File=VERSION.md
-VN="$(command | grep -oP '(?<=VERSION:).*' "$File";) "
-
-if [ -d "$File" ]; 
-	then
-                sleep 5
-    		whiptail --title "Mate Setup Wizard" --msgbox "Mate is already installed. V $VN Click OK To return to main menu." 10 60
-		wget -O - "https://raw.githubusercontent.com/kangadesk/mate-desktop/master/mate_setup.sh" | sudo bash
-	else
-		sleep 5
-		wget -O - "https://raw.githubusercontent.com/kangadesk/mate-desktop/master/packages/install.sh" | sudo bash
-fi
-
-
-
-
 cd /opt/kangadesk/
 File=VERSION.md
 if grep -q "V:0.0.1" "$File";
