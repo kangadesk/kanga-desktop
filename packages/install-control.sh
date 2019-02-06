6995f40b95c1a28c8f681e7628c1751d66ccac48
teamKangadesk <team@kangadesk.com>
2019-02-06 02:09:03 -0500

    Update install-control.sh

--- packages/install-control.sh
+++ packages/install-control.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#Run As Root
-if [[ $EUID -ne 0 ]]; then
-   echo "Please run setup as root." 
-   exit 1
-fi
-#
-
 cd /etc/
 File="os-release"
 if grep -q "bian" "$File";
@@ -14,8 +7,8 @@ if grep -q "bian" "$File";
 		sleep 5
 	else
 		sleep 5
-    		whiptail --title "Mate Setup Wizard" --msgbox "System not supported. Click OK To return to main menu." 10 60
-    		wget -O - "https://raw.githubusercontent.com/pilelu/mate-desktop/master/mate_setup.sh" | sudo bash
+    		whiptail --title "Mate Setup Wizard" --msgbox "System not supported. Click OK To exit." 10 60
+    		exit
 fi
 
 cd /opt/mate/
