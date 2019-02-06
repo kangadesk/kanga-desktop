f5802c181eeb9f67d1c0896a42e8d9b82c79694b
teamKangadesk <team@kangadesk.com>
2019-02-06 02:12:31 -0500

    Update version-control.sh

--- updater/version-control.sh
+++ updater/version-control.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 
-#Run As Root
-if [[ $EUID -ne 0 ]]; then
-   echo "Please run setup as root." 
-   exit 1
-fi
-#
-
-cd /etc/
-File=os-release
-if grep -q "bian" "$File";
-	then
-		sleep 5
-	else
-		sleep 5
-    		whiptail --title "Mate Setup Wizard" --msgbox "System not supported. Click OK To return to main menu." 10 60
-    		wget -O - "https://raw.githubusercontent.com/pilelu/mate-desktop/master/mate_setup.sh" | sudo bash
-fi
-
 {
     for ((i = 0 ; i <= 100 ; i+=20)); do
         sleep 1
         echo $i
     done
 }| whiptail --gauge "Checking For Updates" 6 60 0    
+
 
 cd /opt/mate/
 File=VERSION.md
